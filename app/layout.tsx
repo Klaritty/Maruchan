aee44798ceaf69d2600a807e28473468e8191bf9
klaritty <lucas.giorgi@klaritty.com>
2023-09-07 18:37:51 -0300

    confugure eslint and prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,23 @@
-import NavBar from "@/components/NavBar";
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import Footer from "@/components/Footer";
+import NavBar from '@/components/NavBar'
+import './globals.css'
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import Footer from '@/components/Footer'
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Maruchan México",
-};
+  title: 'Maruchan México'
+}
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body className={inter.className}>
-        <NavBar/>
+        <NavBar />
         {children}
-        <Footer/>
+        <Footer />
       </body>
     </html>
-  );
+  )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import React from "react"
+import React from 'react'
 
 export default function Home() {
   return (
     <>
-      <h1 className="bg-[#1e0c0c] text-white">Home</h1>
+      <h1 className='bg-[#1e0c0c] text-white'>Home</h1>
     </>
   )
 }
