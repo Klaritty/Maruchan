31565d66c847f827ca88f40d43226808b0ff9826
klaritty <lucas.giorgi@klaritty.com>
2023-09-08 14:39:22 -0300

    pruebas

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import NavBar from '@/components/NavBar'
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <>
-      <h1 className='bg-[#1e0c0c] text-white'>Home</h1>
-    </>
+    <div className='bg-[#1e0c0c] w-screen h-screen flex'>
+      <div className=''>
+        <Image src='/images/Layer5.png' alt='' width={400} height={200} />
+      </div>
+      <div className='pt-20'>
+        <h1 className='text-[34px] font-barlow text-white leading-snug w-[317px] h-[223px] '>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit
+        </h1>
+      </div>
+    </div>
   )
 }
